6bcd5c4c219da7ec2690c20b575f4ab8a41ad8c6
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2025-04-07 11:25:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 // main.js
-const frontend = require('./frontend');
 
-frontend.mostrarFormulario();
+import validarYGuardarTarea from "./back-end.js"
+
+function handleform(e) {
+    e.preventDefault()
+    let tareaText = document.getElementById('title').value
+    let tareadesc = document.getElementById('descrip').value
+    let tarea = `${tareaText} ${tareadesc}`
+    let res = validarYGuardarTarea(tarea)
+    showTarea(tarea)
+}
+
+function showTarea  (mensaje) {
+   let container = document.getElementById('tareas-container')
+   let span = document.createElement("span")
+   span.innerHTML = mensaje
+   container.appendChild(span)
+
+} 
+document.getElementById('form').addEventListener('submit', handleform)
